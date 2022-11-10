c36c0675e222f75169baea4a998d05673ebd36be
ida <njiegaye@hotmail.co.uk>
2022-11-10 14:12:23 +0000

    added config files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+
+    // comment
+    public static void main(String[] args) {System.out.println("Hello");
+        // more comments
+
         System.out.println("Hello");
+
     }
 }
